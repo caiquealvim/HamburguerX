084644586162662157e40726feec1fdf1c49bb40
unknown <caique.apd.engenharia@gmail.com>
2024-04-01 19:13:02 -0300

    atualização

--- app/components/Templo/TemploX.tsx
+++ app/components/Templo/TemploX.tsx
@@ -12,8 +12,11 @@ const TemploAdsp = () => {
                 Ajude a Construir o Novo Templo da <span className='text-green-600'>ADSP </span> 
             </h1>
             <p className='opacity-70 w-[65%] md:[50%] mx-auto mt-6 text-white font-semibold'>
-            Jesus olhou e viu os ricos colocando suas contribuições nas caixas de ofertas. Viu também uma viúva pobre colocar duas pequeninas moedas de cobre. E disse: "Afirmo que esta viúva pobre colocou mais do que todos os outros. Todos esses deram do que lhes sobrava; mas ela, da sua pobreza, deu tudo o que possuía para viver".
-            Lucas 21:1-4
+            Jesus olhou e viu os ricos colocando suas contribuições nas caixas de ofertas. 
+            Viu também uma viuva pobre colocar duas pequeninas moedas de cobre. E disse
+            Afirmo que esta viuva pobre colocou mais do que todos os outros. Todos esses deram do que lhes sobrava,
+            mas ela, da sua pobreza, deu tudo o que possuía para viver.
+            Lucas 21 1-4
             </p>
             <div className='w-[80%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center mx-auto pt-8 gap-8'>
                 <div className='flex flex-col items-center gap-4'>
